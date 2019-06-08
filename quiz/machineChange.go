bac0c21e8edb23937e47c09e92035869bb02ffc7
smalldog124 <panumars124@gmail.com>
2019-06-08 15:10:00 +0700

    [Created]:สร้างไฟล์สำหรับทำ quiz

---
+++ quiz/machineChange.go
@@ -0,0 +1,12 @@
+package quiz
+
+/*
+Machine Change Calculator Calculate the change (coins) to return to a customer when they buy something.
+Test case
+| No |    Input      | Output                       |
+-----|---------------|------------------------------|
+	 | Cost | Amount | Change                       |
+  1  | 215  | 300    | [50,20,10,5]                 |
+  2  | 486  | 600    | [100,10,2,2]                 |
+  3  | 12   | 400    | [100,100,100,50,20,10,5,2,1] |
+*/
